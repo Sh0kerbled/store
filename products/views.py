78924a74cc1ade69f98d722de18f48224551315c
Dauren <d.k.nurgaziev@gmail.com>
2026-02-19 11:20:33 +0500

    layout and links added

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 def index(req):
-    return render(req, "index/index.html")
+    return render(req, "products/index.html")
+
+def products(req):
+    return render(req, "products/products.html")
